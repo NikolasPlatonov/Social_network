e3df40e061b341d12ed5c847e863555dd0cd9505
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-01-21 13:41:42 +0200

    Components created inside App file

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>first div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">New feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 };
