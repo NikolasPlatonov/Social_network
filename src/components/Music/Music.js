16f0db851f0e1340cfde1edbcde77ddf07b6ce96
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-01-23 22:25:08 +0200

    Components and Route for Navbar links

---
+++ src/components/Music/Music.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Music.module.css';
+
+const Music = () => {
+  return (
+    <div>
+      <div>MUSIC</div>
+      <div>MUSIC</div>
+      <div>MUSIC</div>
+    </div>
+  );
+};
+
+export default Music;
---
+++ src/components/News/News.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './News.module.css';
+
+const News = () => {
+  return (
+    <div>
+      <div>NEWS</div>
+      <div>NEWS</div>
+      <div>NEWS</div>
+    </div>
+  );
+};
+
+export default News;
