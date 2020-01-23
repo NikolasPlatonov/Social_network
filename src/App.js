f1563727932cf60b85ff7c468db8c2723683d370
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-01-23 23:04:47 +0200

    Navbar list updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Pprofile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music';
-import { Route, BrowserRouter } from 'react-router-dom';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/dialogs" component={Dialogs} />
           <Route path="/news" component={News} />
           <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
         </div>
       </div>
     </BrowserRouter>
---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Settings.module.css';
+
+const Settings = () => {
+  return (
+    <div>
+      <div>SETTINGS</div>
+      <div>SETTINGS</div>
+      <div>SETTINGS</div>
+    </div>
+  );
+};
+
+export default Settings;
