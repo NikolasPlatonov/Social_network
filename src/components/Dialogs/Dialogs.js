6817bb04d8d859e78b33feefe963131c5b952ce9
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-01-23 21:40:24 +0200

    Dialogs component

---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = () => {
+  return (
+    <div>
+      <div>DIALOGS</div>
+      <div>DIALOGS</div>
+      <div>DIALOGS</div>
+    </div>
+  );
+};
+
+export default Dialogs;
