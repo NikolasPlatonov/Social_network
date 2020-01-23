7d6f1e82cdd74e56239dce1a388ba78164bdccce
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-01-23 16:30:08 +0200

    Posts components for Profile layout

--- src/components/Pprofile/MyPosts/MyPosts.js
+++ src/components/Pprofile/MyPosts/MyPosts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
+import Post from './Post/Post';
 
 const MyPosts = () => {
   return (
@@ -8,15 +9,12 @@ const MyPosts = () => {
       <div>
         <textarea></textarea>
         <button>Add post</button>
-        <button>Remove</button>
       </div>
-      <div>
-        <div className={s.item}>Post 1</div>
-        <div>Post 2</div>
-        <div>Post 3</div>
-        <div>Post 4</div>
-        <div>Post 5</div>
-        <div>Post 6</div>
+      <div className={s.posts}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
